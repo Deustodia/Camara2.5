9727d2e7312396dd395fecfba1bee516fb3101d1
mokuzaru <kenny.otoya@pucp.pe>
2019-07-05 02:46:19 -0500

    first

---
+++ Assets/mov_cam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+*This script was made to control de main camara to orbit around a empty gameobject in the Scene
+*
+*Made by Deustodia
+*
+*Suportt us with a suscription at youtube
+*/
+public class mov_cam : MonoBehaviour
+{
+    [Range(1f,10f)]
+    public float speed;
+
+    void Update(){
+      transform.Rotate(0,speed * Time.deltaTime, 0);
+    }
+}
---
+++ Assets/mov_person.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+*This script was made to mantein the player object always in front of a cam 
+*
+*Made by Deustodia
+*
+*Suportt us with a suscription at youtube
+*/
+
+public class mov_person : MonoBehaviour
+{
+    public Transform cam;
+    void Update()
+    {
+        transform.LookAt(cam);
+    }
+}
